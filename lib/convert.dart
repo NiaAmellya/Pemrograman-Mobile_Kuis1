33ab9355965ec92b667a57e5988337ff6b851f4c
NiaAmellya <niaamellya12@gmail.com>
2021-03-16 22:08:18 +0700

    Keterangan Setiap Line

--- lib/convert.dart
+++ lib/convert.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Convert extends StatelessWidget {
+  //widget untuk button Konversi Berat
   Function konvertHandler;
   Convert({Key key, @required this.konvertHandler});
   @override
@@ -10,12 +10,13 @@ class Convert extends StatelessWidget {
       width: double.infinity,
       height: 50,
       child: RaisedButton(
-        onPressed: konvertHandler,
-        color: Colors.green[200],
-        textColor: Colors.white,
+        onPressed:
+            konvertHandler, //mengatur button ketika ditekan akan ada perubahan
+        color: Colors.green[200], // memberi warna hijau pada button
+        textColor: Colors.white, // memberi warna putih pada text
         child: Text(
           "Hitung ",
-          style: TextStyle(fontSize: 20),
+          style: TextStyle(fontSize: 20), // mengatur ukuran font pada text
         ),
       ),
     );
